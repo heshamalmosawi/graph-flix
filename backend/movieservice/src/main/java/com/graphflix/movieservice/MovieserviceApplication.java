a6f35c82a4bff9283e666b22a88f03c3577751b1
heshamalmosawi <hishamalmosawii@gmail.com>
2026-02-19 17:36:44 +0300

    reinitialize movie service to register on eureka

--- backend/movieservice/src/main/java/com/graphflix/movieservice/MovieserviceApplication.java
+++ backend/movieservice/src/main/java/com/graphflix/movieservice/MovieserviceApplication.java
@@ -1,13 +1,33 @@
 package com.graphflix.movieservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
+@RestController
 public class MovieserviceApplication {
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieserviceApplication.class, args);
+	}
+
+	@RequestMapping("/greeting")
+	public String greeting() {
+		return String.format("Hello from %s!", eurekaClient.getApplication(appName).getName());
 	}
 
 }
